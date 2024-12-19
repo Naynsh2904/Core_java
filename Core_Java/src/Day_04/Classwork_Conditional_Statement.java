eac29d7ec9f6f5164f841eb75a97b8a7b582dc67
Nayan Shivankar <shivankarnyn2904@gmail.com>
2024-12-19 23:09:16 +0530

    Adding homework (function)

--- Core_Java/src/Day_04/Classwork_Conditional_Statement.java
+++ Core_Java/src/Day_04/Classwork_Conditional_Statement.java
@@ -1,5 +1,78 @@
 package Day_04;
 
-public class Classwork_Conditional_Statement {
+import java.util.Scanner;
 
+public class Classwork_Conditional_Statement {
+	public static void main(String[] args) {
+//		1) Write a program to display the grades of students based on their percentage using elseif.
+		Scanner scnr = new Scanner(System.in);
+        System.out.print("Enter Your Marks: ");
+        int marks = scnr.nextInt();
+
+        if (marks >= 90 && marks <= 100) {
+            System.out.println("A+");
+        } else if (marks >= 80 && marks < 90) {
+            System.out.println("A");
+        } else if (marks >= 70 && marks < 80) {
+            System.out.println("B+");
+        } else if (marks >= 60 && marks < 70) {
+            System.out.println("B");
+        } else if (marks >= 50 && marks < 60) {
+            System.out.println("C");
+        } else if (marks >= 0 && marks < 50) {
+            System.out.println("Fail");
+        } else {
+            System.out.println("Invalid Marks");
+        }
+        
+//        2) Write a program to check largest among three integer numbers
+        System.out.print("Enter no 1 for check largest among three integer numbers: ");
+        int num1 = scnr.nextInt();
+        System.out.print("Enter no 2 for check largest among three integer numbers: ");
+        int num2 = scnr.nextInt();
+        System.out.print("Enter no 3 for check largest among three integer numbers: ");
+        int num3 = scnr.nextInt();
+        
+        if(num1> num2) {
+        	if(num1> num3) {
+        		System.out.println(num1 +" is greater then "+num2 +" and "+ num3 );
+        	}else {
+        		System.out.println(num3 +" is greater then "+num2 +" and "+ num1 );
+        	}
+        }else if (num2 > num3) {
+        	System.out.println(num2 +" is greater then "+num1 +" and "+ num3 );
+        }else if(num1 < num3 && num2 < num3) {
+    		System.out.println(num3 +" is greater then "+num2 +" and "+ num1 );
+    	} else {
+    		System.out.println("all number is same");
+    	}
+        
+//        3 )Menu Driven program on Arithmetic Operators (Using switch case)
+        System.out.print("enter no 1: ");
+        int ar1 = scnr.nextInt();
+        System.out.print("enter no 2: ");
+        int ar2 = scnr.nextInt();
+        System.out.print("enter No for the given condition addition for(1), substraction for(2), multiplication for(3), division for (4), reminder for(5) ");
+        int symbol = scnr.nextInt();
+        
+        switch (symbol) {
+        case 1 :
+        	System.out.println(ar1 + "+" + ar2+ "=" + (ar1 + ar2));
+        	break;
+        case 2 :
+        	System.out.println(ar1 + "-" + ar2+ "=" + (ar1 - ar2));
+        	break;	
+        case 3 :
+        	System.out.println(ar1 + "*" + ar2+ "=" + (ar1 * ar2));
+        	break;
+        case 4 :
+        	System.out.println(ar1 + "/" + ar2+ "=" + (ar1 / ar2));
+        	break;
+        case 5 :
+        	System.out.println(ar1 + "%" + ar2+ "=" + (ar1 % ar2));
+        	break;	
+        }
+        
+        
+	}
 }
--- Core_Java/src/Day_04/Homework_ConditionalStatement.java
+++ Core_Java/src/Day_04/Homework_ConditionalStatement.java
@@ -1,5 +1,99 @@
 package Day_04;
 
-public class Homework_ConditionalStatement {
+import java.util.Scanner;
 
+public class Homework_ConditionalStatement {
+	public static void main(String[] args) {
+//		Write a program to take the basic salary of employee, add bonus % according to their basic salary and display the gross salary.
+		Scanner scnr = new Scanner(System.in);
+		System.out.print("Enter your basic salary: ");
+		int basic = scnr.nextInt();
+		
+		double gross;
+		
+	    if (basic > 100000) {
+            gross = basic + (basic * 0.50);
+        } else if (basic > 85000) {
+            gross = basic + (basic * 0.40);
+        } else if (basic > 70000) {
+            gross = basic + (basic * 0.30);
+        } else if (basic > 50000) {
+            gross = basic + (basic * 0.20);
+        } else if (basic > 40000) {
+            gross = basic + (basic * 0.15);
+        } else if (basic > 30000) {
+            gross = basic + (basic * 0.10);
+        } else {
+            gross = basic;
+        }
+
+        System.out.println("Your Gross Salary is: " + gross);
+        
+//      Menu Driven program for Area of Circle, Area of Triangle, Circumference of Circle and Perimeter of Triangle. (Using switch case)
+        System.out.println("Enter A number for Area of Circle(1), Area of Triangle(2), Circumference of Circle(3) and Perimeter of Triangle(4). ");
+        int check = scnr.nextInt();
+        float pi = 3.14f;
+        
+        switch (check) {
+        case 1 :
+        	System.out.print("Enter radius of circle: ");
+        	float rad = scnr.nextFloat();
+        	System.out.println("Area of circle is "+ (pi * rad * rad));
+        	break;
+        	
+        case 2 :
+        	System.out.print("Enter radius of circle: ");
+        	float radi = scnr.nextFloat();
+        	System.out.println("Circumference of circle is "+ (pi * radi * 2));
+        	break;
+        	
+        case 3 :
+        	System.out.print("Enter Base of Triangle: ");
+        	float base = scnr.nextFloat();
+        	System.out.print("Enter height of Triangle: ");
+        	float Height = scnr.nextFloat();
+        	System.out.println("Circumference of circle is "+ (0.5 * base * Height));
+        	break;
+        	
+        case 4 :
+        	System.out.print("Enter lenght of 1st side of Triangle: ");
+        	float a = scnr.nextFloat();
+        	System.out.print("Enter lenght of 2nd side of Triangle: ");
+        	float b = scnr.nextFloat();
+        	System.out.print("Enter lenght of 3rd side of Triangle: ");
+        	float c = scnr.nextFloat();
+        	System.out.println("Perimeter of a Triangle is "+ (a + b+ c));
+        	break;
+        
+        default :
+        	System.out.println("Enter valid Number according to a Area of Circle(1), Area of Triangle(2), Circumference of Circle(3) and Perimeter of Triangle(4)");
+        }
+        
+//        Write a program to check largest among four integer numbers
+        System.out.print("Enter num1 for check largest among four integer numbers: ");
+        int num1 = scnr.nextInt();
+        System.out.print("Enter num2 for check largest among four integer numbers: ");
+        int num2 = scnr.nextInt();
+        System.out.print("Enter num3 for check largest among four integer numbers: ");
+        int num3 = scnr.nextInt();
+        System.out.print("Enter num4 for check largest among four integer numbers: ");
+        int num4 = scnr.nextInt();
+        
+        if ((num1 > num2) && (num1 > num3) && (num1 > num4)) {
+        	System.out.println(num1 +"is greater then " + num2 + ", " + num3 + ", "+ num4);
+        	
+        } else if ((num2 > num1) && (num2 > num3) && (num2 > num4)) {
+        	System.out.println(num2 +"is greater then " + num1 + ", " + num3 + ", "+ num4);
+        	
+        } else if ((num3 > num1) && (num3 > num2) && (num3 > num4)) {
+        	System.out.println(num3 +"is greater then " + num2 + ", " + num1 + ", "+ num4);
+        	
+        }else if ((num4 > num1) && (num4 > num2) && (num4 > num3)) {
+        	System.out.println(num4 +"is greater then " + num2 + ", " + num3 + ", "+ num1);
+        	
+        }else {
+        	System.out.println("All Numbers are same");
+        	
+        }
+	}
 }
